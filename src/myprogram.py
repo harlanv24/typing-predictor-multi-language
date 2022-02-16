8d8d545057cba94010bed2433675cfef4faf169d
Gantcho <gantcho@uw.edu>
2022-02-15 20:58:58 -0800

    Hopefully fixed saving and loading

--- src/myprogram.py
+++ src/myprogram.py
@@ -12,32 +12,29 @@ from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import pickle
 
 
-lstm_dim = 128
-epochs = 10
-dropout = 0.1
-seq_len = 100
-bs = 64
-chars_to_id = dict()
-id_to_chars = dict()
+
 
 def load_training_data():
     with open('data/test_data.txt', encoding='UTF-8') as f:
         text = f.read()
     chars = sorted(list(set(text)))
+    chars_to_id = dict()
+    id_to_chars = dict()
     for i, c in enumerate(chars):
         chars_to_id[c] = i
         id_to_chars[i] = c
 
     train_X = []
     train_Y = []
+    seq_len = 100
     for i in range(len(text)-seq_len):
         train_X.append([chars_to_id[c] for c in text[i:i+seq_len]])
         train_Y.append([chars_to_id[c] for c in text[i+seq_len]])
     train_X = np.reshape(train_X, (len(train_X), seq_len, 1))
     train_Y = np_utils.to_categorical(train_Y)
 
-    return train_X, train_Y
-    
+    return train_X, train_Y, chars_to_id
+
     '''
     vocab = set(text)
     print(len(vocab))
@@ -54,18 +51,6 @@ def load_training_data():
     return data_pairs
     '''
 
-def run_pred(model, data):
-    # your code here
-    preds = []
-    all_chars = string.ascii_letters
-    for inp in data:
-        inp_to_id = [chars_to_id[c] for c in inp]
-        inp_to_id = np.reshape(inp_to_id, (1, seq_len, 1))
-        # this model just predicts a random character each time
-        top_guesses = [model.predict(inp_to_id) for _ in range(3)]
-        preds.append(''.join(top_guesses))
-    return preds
-
 
 def write_pred(preds, fname):
     with open(fname, 'wt') as f:
@@ -76,13 +61,17 @@ class MyModel():
     """
     This is a starter model to get you started. Feel free to modify this file.
     """
-    def __init__(self, input_dim, output_dim, dense_dim):
+    def __init__(self, input_dim, output_dim, dense_dim, chars_to_id):
         self.model = Sequential()
-        self.model.add(LSTM(lstm_dim, input_shape=(input_dim, output_dim)))
-        self.model.add(Dropout(dropout))
+        self.model.add(LSTM(128, input_shape=(input_dim, output_dim)))
+        self.model.add(Dropout(0.1))
         self.model.add(Dense(dense_dim, activation = 'softmax'))
         self.model.compile(optimizer = 'adam', loss = 'categorical_crossentropy')
-        self.vocab = None
+        self.epochs = 10
+        self.seq_len = 100
+        self.bs = 64
+        self.chars_to_id = chars_to_id
+
 
     @classmethod
     def load_test_data(cls, fname):
@@ -96,15 +85,27 @@ class MyModel():
 
     def run_train(self, data_X, data_Y, work_dir):
         # your code here 
-        self.model.fit(data_X, data_Y, batch_size = bs, epochs = epochs, verbose = 2)
+        self.model.fit(data_X, data_Y, batch_size = self.bs, epochs = self.epochs, verbose = 2)
     
+    def run_pred(self, data):
+        # your code here
+        preds = []
+        all_chars = string.ascii_letters
+        for inp in data:
+            inp_to_id = [self.chars_to_id[c] for c in inp]
+            inp_to_id = np.reshape(inp_to_id, (1, self.seq_len, 1))
+            # this model just predicts a random character each time
+            top_guesses = [self.model.predict(inp_to_id) for _ in range(3)]
+            preds.append(''.join(top_guesses))
+        return preds
 
     def save(self, work_dir):
         # your code here
         path = os.path.join(work_dir, 'trained_model')
         # save = tf.keras.callbacks.ModelCheckpoint(filepath = path, save_weight_only=True)
         # save(self.model)
-        self.model.save(path)
+        with open(path, 'wb') as f:
+            pickle.dump(self, f)
         # this particular model has nothing to save, but for demonstration purposes we will save a blank file
         # with open(os.path.join(work_dir, 'model.checkpoint'), 'wt') as f:
             # f.write('dummy save')
@@ -113,7 +114,8 @@ class MyModel():
     def load(cls, work_dir):
         # your code here
         path = os.path.join(work_dir, 'trained_model')
-        return tf.saved_model.load(path)
+        with open(path, 'rb') as f:
+            pickle.load(f)
         # this particular model has nothing to load, but for demonstration purposes we will load a blank file
         # with open(os.path.join(work_dir, 'model.checkpoint')) as f:
             # dummy_save = f.read()
@@ -135,9 +137,9 @@ if __name__ == '__main__':
             print('Making working directory {}'.format(args.work_dir))
             os.makedirs(args.work_dir)
         print('Loading training data')
-        train_X, train_Y = load_training_data()
+        train_X, train_Y, chars_to_id = load_training_data()
         print('Instatiating model')
-        model = MyModel(train_X.shape[1], train_X.shape[2], train_Y.shape[1])
+        model = MyModel(train_X.shape[1], train_X.shape[2], train_Y.shape[1], chars_to_id)
         print('Training')
         model.run_train(train_X, train_Y, args.work_dir)
         print('Saving model')
